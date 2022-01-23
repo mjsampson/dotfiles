7209ad720e65f182799f773f68160dca40effd36
Matthew Sampson <matthew@graywolfai.com>
2022-01-23 19:22:07 -0400

    removed run_once_install.sh and using install.js instead.
    It was annoying having all those brew install commands run everytime I ran
    `chezmoi apply`. This avoids that

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+brew install ripgrep
+brew install vagrant
+brew install python
+python3 -m pip install --user powerline-status
+brew install fzf
+brew install bat
+brew install lazygit
+brew install pre-commit
+brew install go
+brew install npm
+npm install -g marked
+brew install graphviz
+brew install shellcheck
+brew install pngpaste
+brew install pyenv
